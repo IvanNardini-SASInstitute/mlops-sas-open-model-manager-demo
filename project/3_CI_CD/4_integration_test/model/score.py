e568f37f6efc4e1e2110d0a19fff48ebbe29ee27
ivnard <ivan.nardini@sas.com>
2020-02-27 21:01:05 +0100

    Test global

--- project/3_CI_CD/4_integration_test/model/score.py
+++ project/3_CI_CD/4_integration_test/model/score.py
@@ -3,28 +3,25 @@ import pandas as pd
 import joblib
 import settings
 
-print(settings.pickle_path)
-
 def score (CLAGE, CLNO, DEBTINC, DELINQ, DEROG, JOB, LOAN, MORTDUE, NINQ, REASON, VALUE, YOJ):
-    print('suca')
-#     "Output: P_BAD0, P_BAD1"
+    "Output: P_BAD0, P_BAD1"
     
-#     try:
-#        _thisModelFit
-#     except NameError:
-#         with open(settings.pickle_path + "rfor_pipeline.pickle", 'rb') as _pFile:
-#             _thisModelFit = joblib.load(_pFile)
+    try:
+       _thisModelFit
+    except NameError:
+        with open(settings.pickle_path + "rfor_pipeline.pickle", 'rb') as _pFile:
+            _thisModelFit = joblib.load(_pFile)
 
-#     # Construct the input array for scoring (the first term is for the Intercept)
-#     input_array = pd.DataFrame([[CLAGE, CLNO, DEBTINC, DELINQ, DEROG, JOB, LOAN, MORTDUE, NINQ, REASON, VALUE, YOJ]],
-#         columns = ['CLAGE', 'CLNO', 'DEBTINC', 'DELINQ', 'DEROG', 'JOB', 'LOAN', 'MORTDUE', 'NINQ', 'REASON', 'VALUE', 'YOJ'])
-# #     ,dtype = float
+    # Construct the input array for scoring (the first term is for the Intercept)
+    input_array = pd.DataFrame([[CLAGE, CLNO, DEBTINC, DELINQ, DEROG, JOB, LOAN, MORTDUE, NINQ, REASON, VALUE, YOJ]],
+        columns = ['CLAGE', 'CLNO', 'DEBTINC', 'DELINQ', 'DEROG', 'JOB', 'LOAN', 'MORTDUE', 'NINQ', 'REASON', 'VALUE', 'YOJ'])
+#     ,dtype = float
 
-#     # Calculate the predicted probabilities
-#     _predProb = _thisModelFit.predict_proba(input_array)
+    # Calculate the predicted probabilities
+    _predProb = _thisModelFit.predict_proba(input_array)
 
-#     # Retrieve the event probability
-#     P_BAD0 = float(_predProb[0][0])
-#     P_BAD1 = float(_predProb[0][1])
+    # Retrieve the event probability
+    P_BAD0 = float(_predProb[0][0])
+    P_BAD1 = float(_predProb[0][1])
 
-#     return(P_BAD0, P_BAD1)
+    return(P_BAD0, P_BAD1)
